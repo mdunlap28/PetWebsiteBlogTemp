0fe70d75c85b20556aa6da8f1e132a4fda1db859
Marcey Dunlap <mdunlap@umw.edu>
2013-02-23 14:01:29 -0500

    Owner Registration

---
+++ 350Project/RegisterMinion.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Forever Home</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="logo">
+	<h1><a href="#">Forever <i> Home</i></a></h1>
+	<h2><span>By Free CSS Templates</span></h2>
+</div>
+<div id="content">
+	<div id="sidebar">
+		<div id="menu">
+			<ul>
+				<li class="active"><a href="#" title="">Homepage</a></li>
+				<li><a href="#" title="">About Us</a></li>
+				<li><a href="RegisterMinion.php" title="">Place Minion for Adoption</a></li>
+				<li><a href="#" title="">Find a Minion</a></li>
+				<li><a href="#" title="">Adopt a Minion</a></li>
+				<li><a href="#" title="">Support</a></li>
+			</ul>
+		</div>
+		<div id="login" class="boxed">
+			<h2 class="title">User Account</h2>
+			<div class="content">
+				<form id="form1" method="post" action="#">
+					<fieldset>
+					<legend>Sign-In</legend>
+					<label for="inputtext1">User ID:</label>
+					<input id="inputtext1" type="text" name="inputtext1" value="" />
+					<label for="inputtext2">Password:</label>
+					<input id="inputtext2" type="password" name="inputtext2" value="" />
+					<input id="inputsubmit1" type="submit" name="inputsubmit1" value="Sign In" />
+					<p><a href="#">Forgot your password?</a></p>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+		
+	</div>
+	<div id="main">
+		<div id="Register your Minion" class="post">
+			<p><img src="images/pets2.jpg" alt="" width="500" height="300" /></p>
+			<h2 class="title">Owner Registration</h2>
+			<!-- Owner Registration page -->
+			
+			<form method = "post" action = "OwnerInfo.php">
+					<table>
+					<tr><td>First Name</td><td><input type="text" id="firstName" name="firstName" /></td></tr>
+					<tr><td>Last Name</td><td><input type="text" id="lastName" name="lastName"/></td></tr>
+					<tr><td>Address</td><td><input type="text" id="address" name="address"/></td></tr>
+					<tr><td>City</td><td><input type="text" id="city" name="city"/></td></tr>
+					<tr><td>State</td><td><input type="text" id="state" name="state"/></td></tr>
+					<tr><td>Phone Number</td><td><input type="text" id="phoneNum" name="phoneNum"/></td></tr>
+					<tr><td>E-mail</td><td><input type="text" id="email" name="email"/></td></tr>
+					
+					<tr><td>Type of Minion</td><td><!--<input type="text" id="petType" name="petType"/>-->
+					
+					<select name="type">
+					<option>Cat</option>
+					<option>Dog</option>
+					<option>Exotic</option>
+					</select></td></tr>
+
+					<tr><td>Username</td><td><input type="text" id="username" name="username"/></td></tr>
+					<tr><td>Password</td><td><input type="text" id="password" name="password"/></td></tr>
+					<tr><td>Re-Enter Password</td><td><input type="text" id="check" name="check"/></td></tr>
+					
+					<tr><td>&nbsp;</td><td><input type="submit" value="Register" /></td></tr>
+					</table>
+					
+					</form>
+			
+			
+		</div>
+		
+			
+		</div>
+	</div>
+</div>
+<div id="footer">
+	<p id="legal">Copyright &copy; 2013 Forever Home. All Rights Reserved. Designed by <a href="http://www.freecsstemplates.org">FCT</a>.</p>
+	<p id="links"><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
+</div>
+</body>
+</html>
